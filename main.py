78d5388fd3aae14c1104d7f6b118cdc232f52479
Brendqnn <bbbird226@gmail.com>
2024-01-26 22:10:38 -0600

    more play options

--- main.py
+++ main.py
@@ -9,56 +9,96 @@ bot = commands.Bot(command_prefix="/", intents=discord.Intents.all())
 
 queue = []
 
+idx = 0
+
+YDL_OPTIONS = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'aac',
+        'preferredquality': '320', # Stream with 320 kbps
+    }],
+}
+
+FFMPEG_OPTIONS = {
+    'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
+    'options': '-vn -b:a 320k',
+}
+
 @bot.command(name="play", help="Play audio from a SoundCloud link.")
 async def play(bot, url):
     if bot.voice_client is None or not bot.voice_client.is_connected():
         await join_vc(bot)
-
-    ydl_opts = {
-        'format': 'bestaudio/best',
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': '192',
-        }],
-    }
-
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    
+    with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
         info_dict = ydl.extract_info(url, download=False)
 
         if 'entries' in info_dict:  # Check if it's a playlist
             entries = info_dict['entries']
-            print(entries)
             queue.extend(entries)
-            await play_next(bot)
+            await play_next(bot, idx)
+
         else:  # It's a single track
             url2 = info_dict['formats'][0]['url']
-            FFMPEG_OPTIONS = {
-                'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
-                'options': '-vn',
-            }
             bot.voice_client.play(discord.FFmpegPCMAudio(url2, **FFMPEG_OPTIONS))
 
-async def play_next(bot):
-    if len(queue) > 1:
-        entry = queue[0]
+async def play_next(bot, idx):
+    if len(queue) > 0:
+        size = len(queue)
+        current_idx = idx    
+        entry = queue[current_idx]
         url = entry['formats'][0]['url']
-        FFMPEG_OPTIONS = {
-            'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
-            'options': '-vn',
-        }
-        queue.pop(0)
-        # Use after parameter to recursively call the play_next method for each track
-        bot.voice_client.play(discord.FFmpegPCMAudio(url, **FFMPEG_OPTIONS), after=lambda e: asyncio.run(play_next(bot)))  
-    else:
-        entry = queue[0]
-        url2 = entry['formats'][0]['url']
-        FFMPEG_OPTIONS = {
-            'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
-            'options': '-vn',
-        }
-        bot.voice_client.play(discord.FFmpegPCMAudio(url2, **FFMPEG_OPTIONS))
+        bot.voice_client.play(discord.FFmpegPCMAudio(url, **FFMPEG_OPTIONS), 
+                              after=lambda e: asyncio.run(play_next(bot, idx + 1)))
 
+    elif idx == size - 1: # Play the last song in the playlist
+        current_idx = idx
+        entry = queue[current_idx]
+        url = entry['formats'][0]['url']
+        bot.voice_client.play(discord.FFmpegPCMAudio(url, **FFMPEG_OPTIONS))
+
+
+async def get_title_from_url(url):
+    ydl_opts = {'quiet': True}
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        try:
+            info_dict = ydl.extract_info(url, download=False)
+            if info_dict and 'title' in info_dict:
+                return info_dict['title']
+        except Exception as e:
+            print(f"Error getting title for {url}: {e}")
+    return "Unknown Title"
+
+@bot.command(name="repeat")
+async def repeat(bot):
+    pass
+
+@bot.command(name="next")
+async def next(bot):
+    global idx
+    if len(queue) - 1 > idx and bot.voice_client.is_playing():
+        bot.guild.voice_client.stop()
+        idx += 1
+        await play_next(bot, idx)
+    else:
+        await bot.send("The last track in queue is currently playing.")
+
+# */ TODO */
+@bot.command(name="back")
+async def back(bot):
+    global idx
+    if idx > 0 and bot.voice_client.is_playing():
+        await pause(bot)
+        idx -= 1
+        await play_next(bot, idx)
+
+@bot.command(name="stop")
+async def stop(bot):
+    voice_channel = bot.guild.voice_client
+    if voice_channel.is_playing():
+        queue.clear()
+        voice_channel.stop()
+             
 @bot.command(name="pause")
 async def pause(bot):
     voice_channel = bot.guild.voice_client
@@ -82,7 +122,7 @@ async def leave_vc(bot):
     if bot.voice_client is not None and bot.voice_client.is_connected():
         await bot.voice_client.disconnect()
     else:
-        await bot.send("Im am not connected to a voice channel.")
+        await bot.send("connected to a voice channel.")
 
 def main():
     @bot.event
